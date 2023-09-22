fa696e52d64d2daecb52995fc7dc326c7be04725
zhangbin0301 <zhangbin0301@qq.com>
2023-09-23 07:57:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-## ==========设置各参数（不需要的可以删掉或者前面加# ）==================
+## ===========================================设置各参数（不需要的可以删掉或者前面加# ）=============================================
 
-# 设置ARGO参数 (如果设置，注意把前面的# 去掉，如果不设置，默认启用临时隧道)
+# 设置ARGO参数 (不设置默认使用临时隧道，如果设置把前面的#去掉)
 # export TOK='xxxxx'
 # export ARGO_DOMAIN='xxxxx'
 
 #设置哪吒参数(NEZHA_TLS='1'开启tls,设置其他关闭tls)
-export NEZHA_SERVER='xxxx'
+export NEZHA_SERVER='data.841013.xyz'
 export NEZHA_KEY='xxx'
 export NEZHA_PORT='443'
 export NEZHA_TLS='1'
 
 # 下面设置UUID和路径，CF_IP是优选IP，SUB_NAME为节点名称
-export UUID='fd80f56e-93f3-4c85-b2a8-c77216c509a7'
+export UUID='ea4909ef-7ca6-4b46-bf2e-6c07896ef338'
 export VPATH='vls'
-export CF_IP='cdn.xn--b6gac.eu.org'
-export SUB_NAME='bbn'
+export CF_IP='www.jjtyss.com'
+export SUB_NAME=''
+
+
+#设置订阅上传地址
+export SUB_URL='https://newbirds-sub.hf.space/upload-ea4909ef-7ca6-4b46-bf2e-6c07896ef338'
+
+# 设置启动玩具原程序，senver.jar 原启动文件改名
+# export JAR_SH='java -Xms128M -XX:MaxRAMPercentage=95.0 -jar senver.jar'
+# 默认启动原游戏后，不显示日志
 
 ## ===========================================参数设置完毕，启动程序===============================
 if command -v curl &>/dev/null; then
@@ -35,4 +43,4 @@ else
 $DOWNLOAD_CMD https://github.com/dsadsadsss/plutonodes/releases/download/xr/main-arm > /tmp/app
 fi
 
-chmod 777 /tmp/app && /tmp/app 
+chmod 777 /tmp/app && /tmp/app
